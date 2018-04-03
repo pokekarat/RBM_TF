db415b3039e3bbaedd0606016e0dfc3b5902c342
pokekarat <pokekarat@gmail.com>
2018-04-03 10:32:54 +0700

    First commit

---
+++ rbm_test.py
@@ -0,0 +1,13 @@
+from rbm import RBM
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+#Loading in the mnist data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+RBM_visible_sizes = 784
+RBM_hidden_sizes = 600
+
+rbm = RBM(RBM_visible_sizes, RBM_hidden_sizes,verbose=1)
+rbm.fit(trX,teX)
